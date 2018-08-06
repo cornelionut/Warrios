151577b5b5d787a3f80345fa113250f75e2dd407
cornelionut <cornel.ionut95@yahoo.com>
2018-08-06 12:35:16 +0300

    A fost efectuat commit

---
+++ Warriors/Warriors/Warrior.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warriors
+{
+    class Warrior
+    {
+        private int goodGuyStartingHealth;
+        private int badGuyStartingHealth;
+
+        private Faction faction;
+
+        private int health;
+        private string name;
+        private bool isAlive;
+
+        private Weapon weapon;
+        private Armour armour;
+    }
+}
